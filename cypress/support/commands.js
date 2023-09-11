2f6d8d9b33f25313ef0275e43a2772533ff2dfa3
LucasGBarros <lucasgomesdebarros99@gmail.com>
2023-09-11 15:58:59 -0300

    Ajuste de dados conforme layout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,17 @@
 Cypress.Commands.add('basicComand', function(){
     cy.contains('button', 'Aceitar todos Cookies e continue').click() 
-    cy.get('.natds15 > .MuiIconButton-label > .material-icons').click()
+    cy.get('.natds16 > .MuiIconButton-label > .material-icons').click()
 })
 
 Cypress.Commands.add('selectProduct', function(){
-    cy.get('.MuiInputBase-input').type('Deo Parfum Natura Homem Emocion.e{enter}')
+    cy.get('.MuiInputBase-input').type('Deo Parfum Natura Homem Emocion.e')
     cy.get('.MuiTypography-root').contains('Natura Homem Emocion.e Deo Parfum').should('be.visible').click()
 })
 
 Cypress.Commands.add('addToCart', function(){
-    cy.get('.MuiButton-label').contains('Comprar').should('be.visible').click()
+    cy.get('.MuiButton-label').contains('Adicionar').should('be.visible').click()
+    cy.contains('tel', 'Digite o CEP aqui').click()
+    cy.type('24756-430{enter}')
     cy.wait(3000)
 })
 
